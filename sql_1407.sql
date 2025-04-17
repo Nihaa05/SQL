054fbb3ba7ad4eb6f4244c2a262b22daa62999cd
Nihaa05 <148804343+Nihaa05@users.noreply.github.com>
2025-04-16 21:22:37 -0500

    Delete sql_1407.sql

--- sql_1407.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-select users.name, sum(case when distance is null then 0 else distance end) as travelled_distance
-from users 
-left join rides on users.id = rides.user_id
-group by users.id, users.name
-order by travelled_distance desc, name asc
