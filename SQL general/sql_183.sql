aa2d905f6472112489d0b53858607b4ee67d7a88
Nihaa05 <148804343+Nihaa05@users.noreply.github.com>
2025-06-23 09:47:44 -0500

    Add files via upload

--- SQL general/sql_183.sql
+++ SQL general/sql_183.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
-select name as Customers
-from Customers 
-where id not in (select customerId from orders)
+select C.name as Customers from Customers C
+where id not in (select customerId from Orders)
+
+
+select C.name as Customers 
+from Customers C
+left join Orders O on C.id = O.customerId
+where O.id is null
