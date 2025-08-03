22376533b029075fb18328de76e24a1a16668ac5
Niharika Pendem <148804343+Nihaa05@users.noreply.github.com>
2025-08-02 20:31:40 -0500

    Add files via upload

--- SQL general/sql_178.sql
+++ SQL general/sql_178.sql
@@ -1,3 +1,7 @@
-# Write your MySQL query statement below
-select score,dense_rank() over (order by score desc) as 'rank'
- from Scores;
+select score, (select count(distinct Score) 
+from Scores where Score >= s.Score) `Rank`
+from Scores s
+order by Score desc
+
+select score,dense_rank() over(order by score desc) as `rank`
+from scores
