32739c15e79b4a10dc1273789b01c5b184e1b425
Niharika Pendem <148804343+Nihaa05@users.noreply.github.com>
2025-08-02 21:34:01 -0500

    Add files via upload

--- SQL general/sql_181.sql
+++ SQL general/sql_181.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-select e.name as Employee from employee e, employee manager 
-where e.managerid = manager.Id
-and e.salary > manager.salary 
+select e1.name as Employee from employee e1
+join employee e2 on e1.managerId = e2.id
+where e1.salary> e2.salary
+
