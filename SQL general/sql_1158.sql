b48e24b1acf285f7d2e4068bebd613c289bcd9bd
Niharika Pendem <148804343+Nihaa05@users.noreply.github.com>
2025-08-14 18:26:22 -0500

    Add files via upload

--- SQL general/sql_1158.sql
+++ SQL general/sql_1158.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-select user_id as buyer_id,join_date, count(order_date) as orders_in_2019
+select u.user_id as buyer_id , u.join_date , count(o.order_id) as orders_in_2019
 from users u
-left join orders o
-on u.user_id = o.buyer_id
-and year(order_date) = '2019'
-group by u.user_id
+left join orders o on u.user_id = o.buyer_id
+and o.order_date >= '2019-1-1'
+and o.order_date < '2020-1-1'
+group by u.user_id, u.join_date
