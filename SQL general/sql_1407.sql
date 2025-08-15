ecfdc7d3c5c4fed808e70545c09befc3bc9f0902
Niharika Pendem <148804343+Nihaa05@users.noreply.github.com>
2025-08-15 17:47:38 -0500

    Add files via upload

--- SQL general/sql_1407.sql
+++ SQL general/sql_1407.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-select users.name, sum(case when distance is null then 0 else distance end) as travelled_distance
-from users 
-left join rides on users.id = rides.user_id
-group by users.id, users.name
-order by travelled_distance desc, name asc
+select u.name,coalesce(sum(r.distance),0) as travelled_distance
+from users u left join rides r on
+u.id = r.user_id
+group by u.id
+order by travelled_distance desc, u.name
