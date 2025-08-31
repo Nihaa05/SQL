696804256f1b127cb8a234d7e6e3e5c9b79a9516
Niharika Pendem <148804343+Nihaa05@users.noreply.github.com>
2025-08-30 22:50:14 -0700

    Add files via upload

--- select/sql_1683.sql
+++ select/sql_1683.sql
@@ -1,4 +1,3 @@
-/*https://leetcode.com/problems/invalid-tweets/description/?envType=study-plan-v2&envId=top-sql-50*/
 # Write your MySQL query statement below
-select tweet_id from tweets 
-where LENGTH(content) > 15 ;
+select tweet_id from tweets
+where length(content) > 15
