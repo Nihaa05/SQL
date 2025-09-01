c2c5b21038a32989333f3e41644ef2a9a72fd450
Niharika Pendem <148804343+Nihaa05@users.noreply.github.com>
2025-08-31 18:38:45 -0700

    Add files via upload

--- select/sql_610.sql
+++ select/sql_610.sql
@@ -1,6 +1,8 @@
-/*https://leetcode.com/problems/article-views-i/?envType=study-plan-v2&envId=top-sql-50*/
 # Write your MySQL query statement below
-select x,y,z,case when x+y>z and y+z>x and x+z>y then 'Yes'
+select x,y,z,  case when x>0 and y>0 and z>0 and
+(x+y) > z and
+y+z > x and
+z+x > y then 'Yes'
 else 'No'
-end as Triangle
-from Triangle
+end as triangle
+from triangle
